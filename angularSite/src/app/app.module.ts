afb10251fcded6b44c06a09f0c9e454503a13ee8
Brian <brian@harnish.net>
2022-09-29 01:27:11 -0400

    Configure angular

--- angularSite/src/app/app.module.ts
+++ angularSite/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
+import { ApiModule } from './api/api.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ApiModule.forRoot({rootUrl: environment.rootUrl}),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularSite/src/environments/environment.prod.ts
+++ angularSite/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  rootUrl: 'https://03ehz7peki.execute-api.us-east-1.amazonaws.com/Prod',
 };
