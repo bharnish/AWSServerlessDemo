dda39c8590d4030ef1ec88ac4b2b4013700db330
Brian <brian@harnish.net>
2022-09-29 01:12:20 -0400

    CRUD

--- AWSServerlessDemo/Controllers/ValuesController.cs
+++ AWSServerlessDemo/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2.DataModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AWSServerlessDemo.Controllers
@@ -9,36 +10,70 @@ namespace AWSServerlessDemo.Controllers
     [Route("api/[controller]")]
     public class ValuesController : ControllerBase
     {
+        private readonly IDynamoDBContext _repo;
+
+        public ValuesController(IDynamoDBContext repo)
+        {
+            _repo = repo;
+        }
+
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IEnumerable<DbRecord>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var scan = _repo.ScanAsync<DbRecord>(null);
+            return await scan.GetRemainingAsync();
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<DbRecord> Get(string id)
         {
-            return "value";
+            return await _repo.LoadAsync<DbRecord>(id);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public async Task Post([FromBody]StringDto value)
         {
+            var record = new DbRecord
+            {
+                Value = value.Value,
+            };
+
+            await _repo.SaveAsync(record);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public async Task Put(string id, [FromBody]StringDto value)
         {
+            var record = await _repo.LoadAsync<DbRecord>(id);
+            record.Value = value.Value;
+            await _repo.SaveAsync(record);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task Delete(string id)
         {
+            await _repo.DeleteAsync<DbRecord>(id);
         }
+    }
+
+    public class StringDto
+    {
+        public string Value { get; set; }
+    }
+
+#if DEBUG
+    [DynamoDBTable("AWSServerless-Demo")]
+#else
+    [DynamoDBTable("AWSServerlessDemo")]
+#endif
+    public class DbRecord
+    {
+        [DynamoDBHashKey] public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Value { get; set; }
     }
 }
